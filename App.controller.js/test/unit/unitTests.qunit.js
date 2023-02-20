abc56010f4dea587999b84ec14bf6d79175df0bc
cbkwgl <cbk123@gmail.com>
2023-02-20 21:02:39 +0530

    27 QUnit

---
+++ App.controller.js/test/unit/unitTests.qunit.js
@@ -0,0 +1,14 @@
+/* global QUnit */
+
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+  "use strict";
+
+  sap.ui.require(
+    ["sap/ui/demo/walkthrough/test/unit/model/formatter"],
+    function () {
+      QUnit.start();
+    }
+  );
+});
