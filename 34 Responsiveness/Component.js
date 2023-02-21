32d7b7c985ece784b80da010965f15f4a974a0e7
cbkwgl <cbk123@gmail.com>
2023-02-21 23:45:00 +0530

    34 Responsiveness

---
+++ 34 Responsiveness/Component.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+
+		metadata: {
+			interfaces: ["sap.ui.core.IAsyncContentCreation"],
+			manifest: "json"
+		},
+
+		init: function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set data model
+			var oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		}
+	});
+
+});
