4838c83c594df961e637a4e7835f61be1b3a4fea
cbkwgl <cbk123@gmail.com>
2023-02-23 00:55:49 +0530

    37 ARIA

---
+++ 35 Device Adaptability/Component.js
@@ -0,0 +1,35 @@
+sap.ui.define(
+  ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel", "sap/ui/Device"],
+  function (UIComponent, JSONModel, Device) {
+    "use strict";
+
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+      metadata: {
+        interfaces: ["sap.ui.core.IAsyncContentCreation"],
+        manifest: "json",
+      },
+
+      init: function () {
+        // call the init function of the parent
+        UIComponent.prototype.init.apply(this, arguments);
+
+        // set data model
+        var oData = {
+          recipient: {
+            name: "World",
+          },
+        };
+        var oModel = new JSONModel(oData);
+        this.setModel(oModel);
+
+        // set device model
+        var oDeviceModel = new JSONModel(Device);
+        oDeviceModel.setDefaultBindingMode("OneWay");
+        this.setModel(oDeviceModel, "device");
+
+        // create the views based on the url/hash
+        this.getRouter().initialize();
+      },
+    });
+  }
+);
