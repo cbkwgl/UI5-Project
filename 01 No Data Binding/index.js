a5bd6ce9bc89fcf01920b9a3ef645a677a2ebc8c
cbkwgl <cbk123@gmail.com>
2023-02-24 01:25:51 +0530

    03 Property Binding

---
+++ 01 No Data Binding/index.js
@@ -0,0 +1,9 @@
+sap.ui.require(["sap/m/Text"], function (Text) {
+  "use strict";
+
+  // Attach an anonymous function to the SAPUI5 'init' event
+  sap.ui.getCore().attachInit(function () {
+    // Create a text UI element that displays a hardcoded text string
+    new Text({ text: "Hi, my name is I don't know" }).placeAt("content");
+  });
+});
