a4ef63fd87ef153ba7564a6e4366d6e1ad3c79ba
cbkwgl <cbk123@gmail.com>
2023-03-02 21:51:50 +0530

    11 Add Table to Detail Pane

--- OData/09 Detail Pane/controller/App.controller.js
+++ OData/09 Detail Pane/controller/App.controller.js
@@ -240,24 +240,7 @@ sap.ui.define(
       _setDetailArea: function (oUserContext) {
         var oDetailArea = this.byId("detailArea"),
           oLayout = this.byId("defaultLayout"),
-          oOldContext,
           oSearchField = this.byId("searchField");
-
-        if (!oDetailArea) {
-          return; // do nothing when running within view destruction
-        }
-
-        oOldContext = oDetailArea.getBindingContext();
-        if (oOldContext) {
-          oOldContext.setKeepAlive(false);
-        }
-        if (oUserContext) {
-          oUserContext.setKeepAlive(
-            true,
-            // hide details if kept entity was refreshed but does not exists any more
-            this._setDetailArea.bind(this)
-          );
-        }
 
         oDetailArea.setBindingContext(oUserContext || null);
         // resize view
